db98b61c7a753e7fed174ab8a4ca90c8f0400aa1
Seth Morabito <sethm@loomcom.com>
2012-01-24 16:01:31 -0800

    Added psvn

--- init.el
+++ init.el
@@ -151,6 +151,9 @@
 ;; Outline mode
 (add-to-list 'auto-mode-alist '("\\.outline$" . outline-mode))
 
+;; psvn
+(require 'psvn)
+
 ;; yasnippet
 (require 'yasnippet)
 (yas/initialize)
@@ -254,7 +257,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(cssm-indent-level 4)
- '(default-frame-alist (quote ((menu-bar-lines . 1) (background-mode . light))))
  '(ecb-auto-activate t)
  '(ecb-layout-name "left13")
  '(ecb-options-version "2.32")
@@ -315,7 +317,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Menlo"))))
  '(mumamo-background-chunk-major ((t nil)))
  '(mumamo-background-chunk-submode1 ((((class color) (min-colors 8)) nil)))
  '(mumamo-background-chunk-submode2 ((((class color) (min-colors 8)) nil)))
