54b9535df804edb77a5c1b337a58837f68e2b0e0
Seth Morabito <web@loomcom.com>
2012-08-26 22:15:24 -0700

    Package init files

---
+++ el-get/package-init/init-rhtml.el
@@ -0,0 +1,1 @@
+;; RHTML mode
--- init.el
+++ init.el
@@ -138,13 +138,14 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+(setq el-get-user-package-directory "~/.emacs.d/el-get/package-init")
+
 (unless (require 'el-get nil t)
   (url-retrieve
    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
    (lambda (s)
-     (let (el-get-master-branch)
-       (goto-char (point-max))
-       (eval-print-last-sexp)))))
+     (goto-char (point-max))
+     (eval-print-last-sexp))))
 
 (el-get 'sync)
 
