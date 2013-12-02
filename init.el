8e705de9379f7743cfa41eb4a68ceabe18ce9fe3
Seth Morabito <seth.morabito@nominum.com>
2013-12-02 11:01:38 -0800

    Custom variable tweaks

--- init.el
+++ init.el
@@ -407,3 +407,16 @@
     (insert "  <script src=\"js/scripts.js\"></script>\n")
     (insert "</body>\n")
     (insert "</html>\n")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(find-grep-options "-q -I")
+ '(require-final-newline nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
