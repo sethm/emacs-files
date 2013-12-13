7abc6377bc26e80abf3ede6e452848f09672e55c
Seth Morabito <web@loomcom.com>
2013-12-12 21:49:07 -0800

    Added MELPA package archive

--- init.el
+++ init.el
@@ -216,8 +216,8 @@
 ;;
 
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (defvar my-packages '(starter-kit
