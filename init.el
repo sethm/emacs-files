186c39d2f977b3ba24d22b6b12eda06897eb4d9d
Seth Morabito <web@loomcom.com>
2014-02-06 23:43:06 -0800

    org-mode customizations

--- init.el
+++ init.el
@@ -358,10 +358,12 @@
  ;; If there is more than one, they won't work right.
  '(find-grep-options "-q -I")
  '(menu-bar-mode t)
+ '(org-hide-leading-stars t)
  '(require-final-newline nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Source Code Pro"))))
+ '(org-level-1 ((t (:inherit outline-1 :background "dark gray" :foreground "black" :box (:line-width 2 :color "grey75" :style released-button) :height 1.2)))))
