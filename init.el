202faf59d43f7c845ecd383f90a5449ef9c4129e
Seth Morabito <seth.morabito@nominum.com>
2014-03-12 20:47:52 -0700

    Set up desktop save mode

--- init.el
+++ init.el
@@ -169,6 +169,17 @@
 ;; run with scissors and make all custom themes safe by default.
 (setq custom-safe-themes t)
 
+;; Save desktop state.
+(setq desktop-dirname             "~/.emacs.d/local/"
+      desktop-base-file-name      "emacs.desktop"
+      desktop-base-lock-name      "lock"
+      desktop-path                (list desktop-dirname)
+      desktop-save                t
+      desktop-files-not-to-save   "^$" ;reload tramp paths
+      desktop-load-locked-desktop nil)
+
+(desktop-save-mode 1)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Path Stuff
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
