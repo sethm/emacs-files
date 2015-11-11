3efcc1de7ad58fa7f9d0cf7cdc5e2039bf108e10
Seth Morabito <web@loomcom.com>
2015-11-10 20:17:18 -0800

    Switch back to Inconsolata

--- init.el
+++ init.el
@@ -557,7 +557,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "unknown" :family "Inconsolata"))))
  '(org-block-begin-line ((t (:inherit org-meta-line :underline t :slant italic :height 0.75))) t)
  '(org-block-end-line ((t (:inherit org-meta-line :overline t :slant italic :height 0.75))) t)
  '(org-checkbox ((t (:inherit bold))))
