72a5955633428239883c346e8f35621c00143918
Seth Morabito <web@loomcom.com>
2017-08-17 20:13:35 -0700

    Return to 2 spaces for js

--- init.el
+++ init.el
@@ -117,9 +117,9 @@
 
 (add-hook 'js-mode-hook
           '(lambda ()
-             (setq tab-width 4)
-             (setq js-indent-level 4)
-             (setq c-basic-offset 4)))
+             (setq tab-width 2)
+             (setq js-indent-level 2)
+             (setq c-basic-offset 2)))
 
 (add-hook 'js2-mode-hook
           '(lambda ()
