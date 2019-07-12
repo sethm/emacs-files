993d986b3c653d8c483889efaf70de1f41418ea9
Seth Morabito <web@loomcom.com>
2019-07-12 15:23:32 -0700

    Cleanup and frame placement.

--- init.el
+++ init.el
@@ -1,6 +1,30 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; This Emacs initialization file bootstraps all settings from the
 ;; `configuration.org' file, which is in Literate Programming style.
 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Anything below has been overridden by customization.
+;; DO NOT TOUCH IT.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm flycheck-rust racer cargo rust-mode toml-mode haskell-mode htmlize typescript-mode php-mode web-mode lsp-ui lsp-mode company flycheck paredit yasnippet-snippets yasnippet git-gutter magit graphviz-dot-mode ledger-mode excorporate tabbar org-bullets sml-modeline org-plus-contrib use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
