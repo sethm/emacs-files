814ec365ca2b5e30652fca2c1b06166ef4c13811
Seth Morabito <web@loomcom.com>
2019-07-12 20:46:17 -0700

    Org Agenda setup, remove broken org-drill

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm flycheck-rust racer cargo rust-mode toml-mode haskell-mode htmlize typescript-mode php-mode web-mode lsp-ui lsp-mode company flycheck paredit yasnippet-snippets yasnippet git-gutter magit graphviz-dot-mode ledger-mode excorporate tabbar org-bullets sml-modeline org-plus-contrib use-package))))
+    (auto-package-update helm flycheck-rust racer cargo rust-mode toml-mode haskell-mode htmlize typescript-mode php-mode web-mode lsp-ui lsp-mode company flycheck paredit yasnippet-snippets yasnippet git-gutter magit graphviz-dot-mode ledger-mode excorporate tabbar org-bullets sml-modeline org-plus-contrib use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
