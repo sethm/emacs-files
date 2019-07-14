42f56b92109209a23149acb4e4ac711106767d0b
Seth Morabito <web@loomcom.com>
2019-07-14 15:17:17 -0700

    Theme change

--- init.el
+++ init.el
@@ -14,17 +14,3 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (lsp fsm url-http-ntlm spinner-mode spinner lsp-ui lsp-mode excorporate yasnippet-snippets web-mode use-package typescript-mode toml-mode tabbar sml-modeline racer php-mode paredit org-bullets org magit ledger-mode htmlize helm haskell-mode graphviz-dot-mode git-gutter flycheck-rust company cargo auto-package-update))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
