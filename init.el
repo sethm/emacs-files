9d6273979631e282dce3a34e59dc8a92f6eab064
Seth Morabito <web@loomcom.com>
2021-09-30 14:38:36 -0700

    Migrate to org-mode 9.5
    This is a major update of org-mode, which required some changes
    to init.el.
    
    Additionally, I have waffled, once again, on org-adapt-indentation
    and turned it back off. But this comes with the addition of using
    the null character (?\00) as the leading bullet in org-superstar,
    which means I finally like my org-mode headlines.
    
    I may yet make level-1 and level-2 headlines larger, though.

--- init.el
+++ init.el
@@ -6,6 +6,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-to-list 'load-path "~/.emacs.d/vendor/org-mode/lisp")
-(add-to-list 'load-path "~/.emacs.d/vendor/org-mode/contrib/lisp")
+(add-to-list 'load-path "~/.emacs.d/vendor/org-contrib/lisp")
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
